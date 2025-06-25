4427783dc664f17101c28b29c9638b7e7a9fd4c4
Gabriel Henrique <gabrielhenrique1618@gmail.com>
2025-06-25 16:59:29 -0300

    .

--- App Dependent/lib/pages/global.dart
+++ App Dependent/lib/pages/global.dart
@@ -1,1 +1,2 @@
-const String? apiUrl = 'https://02a8-2804-61ac-110b-8200-449-b065-d943-e36e.ngrok-free.app';
+const String? apiUrl =
+    'https://02a8-2804-61ac-110b-8200-449-b065-d943-e36e.ngrok-free.app';
